007154df113085d44133c95fc2d7874172834467
dnhphuong-05 <vnbt0703@gamil.com>
2026-03-20 15:39:31 +0700

    Finalllll

--- frontend-admin/src/environments/environment.prod.ts
+++ frontend-admin/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yourdomain.com/api',
+  apiUrl: 'https://252bie503101-group13-production.up.railway.app/api',
   userUrl: 'https://yourdomain.com',
 };
